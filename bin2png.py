a370c6e979b8aa34eeda02fb1f353701a4674322
Evan Sultanik <evan@sultanik.com>
2014-03-28 09:57:35 -0400

    Added command line options to specify the dimensions.

--- bin2png.py
+++ bin2png.py
@@ -26,14 +26,31 @@ class FileReader(object):
     def __iter__(self):
         return iter(self.file)
 
-def choose_file_dimensions(infile):
+def choose_file_dimensions(infile, input_dimensions = None):
+    if input_dimensions is not None and len(input_dimensions) >= 2 and input_dimensions[0] is not None and input_dimensions[1] is not None:
+        # the dimensions were already fully specified
+        return input_dimensions
     if not isinstance(infile, FileReader):
         infile = FileReader(infile)
     num_bytes = len(infile)
     num_pixels = int(math.ceil(float(num_bytes) / 3.0))
     sqrt = math.sqrt(num_pixels)
     sqrt_max = int(math.ceil(sqrt))
-    dimensions = (sqrt_max, sqrt_max)
+
+    if input_dimensions is not None and len(input_dimensions) >= 1:
+        if input_dimensions[0] is not None:
+            # the width is specified but the height is not
+            if num_pixels % input_dimensions[0] == 0:
+                return (input_dimensions[0], num_pixels / input_dimensions[0])
+            else:
+                return (input_dimensions[0], num_pixels / input_dimensions[0] + 1)
+        else:
+            # the height is specified but the width is not
+            if num_pixels % input_dimensions[1] == 0:
+                return (num_pixels / input_dimensions[1], input_dimensions[1])
+            else:
+                return (num_pixels / input_dimensions[1] + 1, input_dimensions[1])
+
     best_dimensions = None
     best_extra_bytes = None
     for i in range(int(sqrt_max), 0, -1):
@@ -53,8 +70,7 @@ def choose_file_dimensions(infile):
     return best_dimensions
 
 def file_to_png(infile, outfile, dimensions = None):
-    if dimensions is None:
-        dimensions = choose_file_dimensions(infile)
+    dimensions = choose_file_dimensions(infile, dimensions)
     img = Image.new('RGB', dimensions)
     pixels = img.load()
     row = 0
@@ -99,10 +115,15 @@ if __name__ == "__main__":
     parser.add_argument('file', type=argparse.FileType('r'), default=sys.stdin, help="the file to encode as a PNG (defaults to '-', which is stdin)")
     parser.add_argument("-o", "--outfile", type=argparse.FileType('w'), default=sys.stdout, help="the output file (defaults to '-', which is stdout)")
     parser.add_argument("-d", "--decode", action="store_true", default=False, help="decodes the input PNG back to a file")
+    parser.add_argument("-w", "--width", type=int, default=None, help="constrain the output PNG to a specific width")
+    parser.add_argument("-v", "--height", type=int, default=None, help="constrain the output PNG to a specific height")
 
     args = parser.parse_args()
 
     if args.decode:
         png_to_file(args.file, args.outfile)
     else:
-        file_to_png(args.file, args.outfile)
+        dimensions = None
+        if args.height is not None or args.width is not None:
+            dimensions = (args.width, args.height)
+        file_to_png(args.file, args.outfile, dimensions=dimensions)
