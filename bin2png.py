99ab8e2ed9a7c3f25901a4e2f7f6aad1275043bf
Evan Sultanik <evan@sultanik.com>
2014-03-27 23:41:23 -0400

    Implemented file length detection and automatic PNG dimension calculation.

--- bin2png.py
+++ bin2png.py
@@ -1,40 +1,67 @@
 #!/usr/bin/env python2
 
 from PIL import Image
+import math
+import StringIO
+import os
+import sys
 
-def get_length(stream):
-    """Gets the number of bytes in the stream."""
-    old_position = stream.tell()
-    stream.seek(0)
-    length = 0
-    while True:
-        r = stream.read(1024)
-        if not r:
-            break
-        length += len(r)
-    stream.seek(old_position)
-    return length
+class FileReader(object):
+    def __init__(self, path):
+        if isinstance(path, file):
+            if path.name == "<stdin>":
+                infile = sys.stdin.read()
+                self.length = len(infile)
+                self.file = StringIO.StringIO(infile)
+            else:
+                self.length = os.path.getsize(path.name)
+                self.file = path
+        else:
+            self.length = os.path.getsize(path)
+            self.file = open(path)
+    def __len__(self):
+        return self.length
+    def __getattr__(self, attr):
+        return getattr(self.file, attr)
+    def __iter__(self):
+        return iter(self.file)
 
 def choose_file_dimensions(infile):
-    length = get_length(infile)
+    if not isinstance(infile, FileReader):
+        infile = FileReader(infile)
+    num_bytes = len(infile)
+    num_pixels = int(math.ceil(float(num_bytes) / 3.0))
+    sqrt = math.sqrt(num_pixels)
+    sqrt_max = int(math.ceil(sqrt))
+    dimensions = (sqrt_max, sqrt_max)
+    for i in range(int(sqrt), 1, -1):
+        if num_pixels % i == 0:
+            dimensions = (i, num_pixels / i)
+            break
+    extra_bytes = dimensions[0] * dimensions[1] * 3 - num_bytes
+    if extra_bytes > 0:
+        sys.stderr.write("Could not find PNG dimensions that perfectly encode %s bytes; the encoding will be tail-padded with %s zeros." % (num_bytes, extra_bytes))
+    return dimensions
 
-def file_to_png(infile, outfile):
-    img = Image.new('RGB', (1024, 1024))
+def file_to_png(infile, outfile, dimensions = None):
+    if dimensions is None:
+        dimensions = choose_file_dimensions(infile)
+    img = Image.new('RGB', dimensions)
     pixels = img.load()
     row = 0
-    column = 0
+    column = -1
     while True:
         b = infile.read(3)
         if not b:
             break
-        
+
         column += 1
         if column >= img.size[0]:
             column = 0
             row += 1
             if row >= img.size[1]:
                 raise Exception("TODO: Write exception!")
-        
+                
         color = [ord(b[0]), 0, 0]
         if len(b) > 1:
             color[1] = ord(b[1])
@@ -47,12 +74,11 @@ def file_to_png(infile, outfile):
 
 if __name__ == "__main__":
     import argparse
-    import sys
     
     parser = argparse.ArgumentParser(description="A simple cross-platform script for encoding any binary file into a lossless PNG.", prog="bin2png")
 
-    parser.add_argument('file', type=argparse.FileType('r'), help="the file to process")
-    parser.add_argument("-o", "--outfile", type=argparse.FileType('w'), default=sys.stdout, help="the output file (default to stdout)")
+    parser.add_argument('file', type=argparse.FileType('r'), default=sys.stdin, help="the file to process (defaults to '-', which is stdin)")
+    parser.add_argument("-o", "--outfile", type=argparse.FileType('w'), default=sys.stdout, help="the output file (defaults to '-', which is stdout)")
 
     args = parser.parse_args()
 
